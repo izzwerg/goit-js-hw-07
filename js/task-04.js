61c0d24c2b30782baf29281e8b3e0719c127f3c6
izzwerg <izzwerg.kr@gmail.com>
2023-12-14 15:20:35 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
+let regForm = document.querySelector('.login-form');
+regForm.addEventListener('submit', regSubmit);
+function regSubmit(e) {
+  e.preventDefault();
+  let form = e.target;
+  let formEmail = form.elements.email.value;
+  let formPass = form.elements.password.value;
 
+  if (formEmail === '' || formPass === '') {
+    alert('All form fields must be filled in');
+  } else {
+    let formReturn = {};
+    formReturn[form.elements.email.name] = form.elements.email.value;
+    formReturn[form.elements.password.name] = form.elements.password.value;
+    console.log(formReturn);
+    form.reset();
+  }
+}
