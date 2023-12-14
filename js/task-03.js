021a19db3ca59d171747189e8d6e0500e0348f0f
izzwerg <izzwerg.kr@gmail.com>
2023-12-14 12:38:20 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,10 +1,9 @@
 let nameIn = document.querySelector('#name-input');
 let nameOut = document.querySelector('#name-output');
 nameIn.addEventListener('input', e => {
-  if (e.currentTarget.value === '') {
+  if (e.currentTarget.value.trim() === '') {
     nameOut.textContent = 'Anonymous';
   } else {
-    nameOut.textContent = e.currentTarget.value;
+    nameOut.textContent = e.currentTarget.value.trim();
   }
-  console.dir(e.currentTarget.value);
 });
