b16881881f72b0b54d318228d1acc95b378ee4cc
izzwerg <izzwerg.kr@gmail.com>
2023-12-15 16:09:59 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -9,5 +9,28 @@ let createButton = document.querySelector('[data-create]');
 let destroyButton = document.querySelector('[data-destroy]');
 let outputArea = document.querySelector('#boxes');
 
+createButton.addEventListener('click', valueCheck);
 
-console.log(outputArea)
+function valueCheck() {
+  let newValue = inputForm.value;
+  if (newValue >= 1 && newValue <= 100) {
+    createBoxes(newValue);
+  }
+}
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  let outCode = '';
+  for (let index = 0; index < amount; index++) {
+    outCode += `<div style = 'background-color:${getRandomHexColor()}; width: ${boxSize}px; height: ${boxSize}px;'></div>`;
+    boxSize += 10;
+  }
+  outputArea.innerHTML = outCode;
+}
+
+destroyButton.addEventListener('click', delSquares);
+
+function delSquares() {
+  outputArea.innerHTML = '';
+  inputForm.value = '';
+}
